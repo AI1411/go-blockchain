9fe9bd12a468c83c318451970c2b6f2677989797
AI1411 <goethe0719@gmail.com>
2020-01-07 18:00:56 +0900

    チェーンが見やすくなるように整形

--- blockchain.go
+++ blockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -21,6 +22,7 @@ func NewBlock(nonce int, previousHash string) *Block {
 	return b
 }
 
+//ブロックの新規作成
 func (b *Block) Print() {
 	fmt.Printf("timestamp         %d\n", b.timestamp)
 	fmt.Printf("nonce             %d\n", b.nonce)
@@ -28,11 +30,44 @@ func (b *Block) Print() {
 	fmt.Printf("transactions      %s\n", b.transactions)
 }
 
+//ブロックチェーンのストラクト作成
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "init hash")
+	return bc
+}
+
+//作成したブロックをプールに追加していく
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i,
+			strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
 func init() {
 	log.SetPrefix("Blockchain")
 }
 func main() {
-	b := NewBlock(0, "init hash")
+	//b := NewBlock(0, "init hash")
 	//fmt.Println(b)
-	b.Print()
+	//b.Print()
+	blockChain := NewBlockchain()
+	//fmt.Println(blockChain)
+	blockChain.Print()
+	blockChain.CreateBlock(5, "hash 1")
+	blockChain.Print()
 }
