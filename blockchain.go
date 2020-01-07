b58edcce54951a3f64aa06caed7c0b130a78c853
AI1411 <goethe0719@gmail.com>
2020-01-07 17:45:11 +0900

    ブロック作成

--- blockchain.go
+++ blockchain.go
@@ -3,12 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 )
 
-func init()  {
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp         %d\n", b.timestamp)
+	fmt.Printf("nonce             %d\n", b.nonce)
+	fmt.Printf("previousHash      %s\n", b.previousHash)
+	fmt.Printf("transactions      %s\n", b.transactions)
+}
+
+func init() {
 	log.SetPrefix("Blockchain")
 }
 func main() {
-	log.Println("test")
-	fmt.Println("test2")
+	b := NewBlock(0, "init hash")
+	//fmt.Println(b)
+	b.Print()
 }
