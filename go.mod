5d46a91d3418493a7ff013878cdbcbdc7cccf828
AI1411 <goethe0719@gmail.com>
2022-04-23 11:12:00 +0900

    公開鍵、秘密鍵生成(エンコード)

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module blockchain
 
 go 1.17
+
+require (
+	github.com/btcsuite/btcutil v1.0.2 // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
+)
--- model/wallet.go
+++ model/wallet.go
@@ -4,19 +4,54 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
 )
 
 type Wallet struct {
-	PrivateKey *ecdsa.PrivateKey
-	PublicKey  *ecdsa.PublicKey
+	PrivateKey        *ecdsa.PrivateKey
+	PublicKey         *ecdsa.PublicKey
+	BlockchainAddress string
 }
 
 func NewWallet() *Wallet {
 	w := new(Wallet)
+	// 公開鍵(64byte)、秘密鍵(32byte)生成
 	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	w.PrivateKey = privateKey
 	w.PublicKey = &w.PrivateKey.PublicKey
+
+	h2 := sha256.New()
+	h2.Write(w.PublicKey.X.Bytes())
+	h2.Write(w.PublicKey.Y.Bytes())
+	digest2 := h2.Sum(nil)
+
+	h3 := ripemd160.New()
+	h3.Write(digest2)
+	digest3 := h3.Sum(nil)
+
+	vd4 := make([]byte, 21)
+	vd4[0] = 0x00
+	copy(vd4[1:], digest3[:])
+
+	h5 := sha256.New()
+	h5.Write(vd4)
+	digest5 := h5.Sum(nil)
+
+	h6 := sha256.New()
+	h6.Write(digest5)
+	digest6 := h6.Sum(nil)
+
+	cSum := digest6[:4]
+	dc8 := make([]byte, 25)
+	copy(dc8[:21], vd4[:])
+	copy(dc8[21:], cSum[:])
+
+	address := base58.Encode(dc8)
+	w.BlockchainAddress = address
+
 	return w
 }
 
